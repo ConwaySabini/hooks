b1bfeac7e590023c6f5a2b36e8f5d30912c58cc9
Ethan Sabini <ConwaySabini@Outlook.com>
2022-10-08 13:51:29 -0700

    useInterval

--- src/components/useFetch.tsx
+++ src/components/useFetch.tsx
@@ -87,3 +87,34 @@ function useFetch<T = unknown>(url?: string, options?: RequestInit): State<T> {
 }
 
 export default useFetch
+
+
+
+const url = `http://jsonplaceholder.typicode.com/posts`
+
+
+interface Post {
+
+    userId: number
+
+    id: number
+
+    title: string
+
+    body: string
+
+}
+
+
+function Component() {
+
+    const { data, error } = useFetch<Post[]>(url)
+
+
+    if (error) return <p>There is an error.</p>
+
+    if (!data) return <p>Loading...</p>
+
+    return <p>{data[0].title}</p>
+
+}
